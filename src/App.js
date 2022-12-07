5177d69dd8ae147c885b002f94ccce7bff70cf47
dfilmon <abraha.filmon@gmail.com>
2022-12-06 19:31:19 -0500

    2 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import './style.css';
 import React from 'react';
 import 'tailwindcss/tailwind.css';
+import Hero from './Hero';
+import Feature from './Feature';
+import MyForm from './MyForm';
 
 export default function App() {
   return (
     <div>
-        Team Hoda
+      <Hero />
+      <Feature />
+      <MyForm />
     </div>
   );
 }
